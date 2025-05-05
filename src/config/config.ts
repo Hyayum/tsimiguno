bcb785c0d71f8346ac8578862af56ab92861d51b
timireno <hinomefi@gmail.com>
2025-05-05 23:29:52 +0900

    環境変数設定

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:8000";
+export const API_URL = "https://tsimiguno-343528282838.asia-northeast1.run.app";
 export const GOOGLE_CLIENT_ID = "343528282838-d8a181km1c5pgeot10lvihoiot0dgn2b.apps.googleusercontent.com";
